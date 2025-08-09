afab5d6639037ab95579b360c1ea1f541a5b9add
Schenry <schenry259@gmail.com>
2025-08-09 15:22:00 -0500

    work on problem

--- problems/min_size_subarray_sum/src/main.rs
+++ problems/min_size_subarray_sum/src/main.rs
@@ -7,7 +7,7 @@ use std::io::{self, Read};
 /// return the minimal length of a subarray whose sum is greater than or equal to target.
 /// If there is no such subarray, return 0 instead.
 ///
-///
+/// Remember that subarray implies contiguous
 ///
 fn main() {
     let mut input = String::new();
@@ -15,7 +15,9 @@ fn main() {
 }
 
 pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
-    0
+    let mut count = 0;
+
+    count
 }
 
 #[cfg(test)]
@@ -34,5 +36,24 @@ mod tests {
     #[test]
     fn testcase_3() {
         assert_eq!(min_sub_array_len(11, vec![1, 1, 1, 1, 1, 1, 1, 1]), 0);
+    }
+
+    #[test]
+    fn testcase_4() {
+        assert_eq!(min_sub_array_len(15, vec![1, 2, 3, 4, 5]), 5);
+    }
+
+    #[test]
+    fn testcase_5() {
+        assert_eq!(min_sub_array_len(7, vec![2, 3, 1, 2, 4, 3]), 2);
+    }
+
+    #[test]
+    fn testcase_6() {
+        assert_eq!(
+            // vec![83, 28, 26, 25, 25, 25, 25, 25, 12, 12, 4, 2]
+            min_sub_array_len(213, vec![12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12]),
+            8
+        );
     }
 }
